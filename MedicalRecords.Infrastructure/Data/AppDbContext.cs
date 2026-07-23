09ba7181e2fec39697a1b26e756434041d5a1fe1
Tsholofelo2607 <tsholofelomotswane773@gmail.com>
2026-07-23 21:52:03 -0200

    Added AppDbContext constructor, connection string, registered DbContext in Program.cs and created initial migration

--- MedicalRecords.Infrastructure/Data/AppDbContext.cs
+++ MedicalRecords.Infrastructure/Data/AppDbContext.cs
@@ -1,10 +1,18 @@
+
 using Microsoft.EntityFrameworkCore;
 using MedicalRecords.Core.Entities;
 
+
 namespace MedicalRecords.Infrastructure.Data
 {
+    
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
     public DbSet<Patient> Patients { get; set; }
     public DbSet<Doctor> Doctors { get; set; }
     public DbSet<MedicalRecord> MedicalRecords { get; set; }
